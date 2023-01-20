8998c0c58345552790e3868d90b80c53e43d1e08
singhkaran202 <103121051@nitt.edu>
2023-01-20 07:36:16 +0530

    Methodology and screenshot section is done and added

--- src/App.js
+++ src/App.js
@@ -13,7 +13,8 @@ export default function App() {
                  who wants to take back control of thier time and be more productive'
             />
 
-            <Card2 
+            <Card2/>
+            {/* <Card2 
                 className='section bg-grey'
                 img={require('./images/iPhone1.jpg')} 
                 // title='Our Values'
@@ -30,9 +31,9 @@ export default function App() {
                 img={require('./images/iPhone3.jpg')} 
                 // title='Our Values'
                 description='After the set limit is reached you can no longer use the app for the day unless you pay the amount.'
-            />
+            /> */}
 
-            <Methodology_card 
+            {/* <Methodology_card 
                 className='section'
                 img='./images/bg.png' 
                 title='Our Methodology' 
@@ -42,7 +43,8 @@ export default function App() {
                         aliquip ex ea commodo consequat. Duis aute irure dolor in
                         reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
                         pariatur.'
-            />
+            /> */}
+            <Methodology_card/>
             <Purpose 
                 className='section'
                 img='./images/bg.png' 
@@ -95,60 +97,114 @@ const Card = (props) =>{
     )
 }
 
-const Card2 = (props) =>{
+const Card2 = () =>{
     return(
-        <div className={props.className} >
-            {/* <div className="small-div">
-                <i className={props.className}></i>
-                <img src={props.img} alt='' width={280} height={139} />
-            </div> */}
+        <div className='card2-div' >
+          <div className="comp1" >
+             <div>
+                <img src={require('./images/iPhone1.jpg')} alt='' className="image-div" />
+             </div>
+             <br/> 
 
-            <div className="big-div">
-            <i className={props.className}></i>
-                
-                {/* <span className='div-title'>
-                    {props.title}
-                </span> */}
-                <span>
-                <img src={props.img} alt='' width={100} height={150} />
+             <div>
+                <span className="des-div">
+                Select your most used apps and set up a daily limit
                 </span>
-                
-            <br/>
-                <span>
-                    {props.description}
+             </div>
+           </div>
+         <div className="comp2"> 
+          <div>
+                <img src={require('./images/iPhone2.jpg')} alt=''  className="image-div"/>
+            </div> 
+             <br/>
+            <div>
+                <span className="des-div">
+                You can put some money at stake to increase your motivation
                 </span>
             </div>
+         </div>
+
+          <div className="comp1 comp3">
+           <div>
+                <img src={require('./images/iPhone3.jpg')} alt=''  className="image-div" />
+            </div> 
+
+            <div>
+                <span className="des-div">
+                After the set limit is reached you can no longer use the app for the day unless you pay the amount.
+                </span>
+            </div>
+          </div>
+          
         </div>
     )
 }
 
 
-const Methodology_card = (props) =>{
+const Methodology_card = () =>{
     return(
-        <div className={props.className} >
-            {/* <div className="small-div">
-                <i className={props.className}></i>
-                <img src={props.img} alt='' width={280} height={139} />
-            </div> */}
+        <div className='method-card' >
+            <div className="title-div">
+                Our Methodology 
+            </div>
 
-            <div className="big-div">
-            <i className={props.className}></i>
-                
-                <span className='div-title'>
-                    {props.title}
-                </span>
-                <span>
-                <img src={props.img} alt='' width={280} height={139} />
-                </span>
-                
-                
-                <span>
-                    {props.description}
-                </span>
+            <div className="second-div">
+              <div className="point1">
+                <div>
+                  <img src={require('./images/hand_icon.png')} alt=''  className="icon-img"/>
+                </div>
+                <div className="point-des">
+                    Spiroc helps you understand how much time you spend on social media, gaming, or other activites that aren't <b>productive</b>.
+                </div>
+              </div>
+
+              <div className="point2">
+               <div>
+                <img src={require('./images/hand_icon.png')} alt=''  className="icon-img"/>
+                </div>
+                <div className="point-des">
+                    The Spiroc community will hold you accountable for your <b>progress</b> so you don't have to feel alone.
+                </div>
+               </div>
+
+               <div className="point3">
+                <div>
+                 <img src={require('./images/hand_icon.png')} alt=''  className="icon-img"/>
+                </div> 
+                <div className="point-des">
+                    You will recieve <b>weekly report</b> that show how much time you spend in each area of your life, so it's easy to see how much time is being spent in each area of your life, so its easy to see how much time is bieng spent on the things that matter most.
+                </div>
+               </div>
             </div>
         </div>
     )
 }
+// const Methodology_card = (props) =>{
+//     return(
+//         <div className={props.className} >
+//             {/* <div className="small-div">
+//                 <i className={props.className}></i>
+//                 <img src={props.img} alt='' width={280} height={139} />
+//             </div> */}
+
+//             <div className="big-div">
+//             <i className={props.className}></i>
+                
+//                 <span className='div-title'>
+//                     {props.title}
+//                 </span>
+//                 <span>
+//                 <img src={props.img} alt='' width={280} height={139} />
+//                 </span>
+                
+                
+//                 <span>
+//                     {props.description}
+//                 </span>
+//             </div>
+//         </div>
+//     )
+// }
 
 
 
